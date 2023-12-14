a5e9151e08b85bad44a84bf539b11a2b27b26af9
DaniSode <danisode@student.chalmers.se>
2023-12-14 17:04:19 +0100

    a

--- Code/models/.ipynb_checkpoints/training-checkpoint.py
+++ Code/models/.ipynb_checkpoints/training-checkpoint.py
@@ -86,7 +86,7 @@ class VQADataset(Dataset):
         sample = {'image': img, 'question': qu2idx, 'question_id': qu_id}
 
         ans2idx = [self.ans_vocab.word2idx(ans) for ans in self.input_data[idx]['valid_ans']]
-        ans2idx = np.random.choice(ans2idxnp, size=1, replace=False)
+        ans2idx = np.random.choice(ans2idx, size=1, replace=False)
         sample['answer'] = ans2idx
         print(sample['answer'])
 
