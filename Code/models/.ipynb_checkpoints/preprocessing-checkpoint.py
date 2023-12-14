3ce4e168a39fdb04a36f1bd1bee45bf14a3ddb32
DaniSode <danisode@student.chalmers.se>
2023-12-14 14:00:41 +0100

    Update preprocessing-checkpoint.py

--- Code/models/.ipynb_checkpoints/preprocessing-checkpoint.py
+++ Code/models/.ipynb_checkpoints/preprocessing-checkpoint.py
@@ -47,242 +47,16 @@ from tensorflow.keras.applications import VGG19
 # In[2]:
 
 
-img_pth = 'dataset\img'
-ann_pth = 'dataset\\ann'
-qst_pth = 'dataset\qst'
-out_img_pth = 'preprocessed\img'
-out_data_pth = 'preprocessed\data'
-out_vocab_pth = 'preprocessed\\vocab'
-out_ann_pth = 'preprocessed\\ann'
-out_qst_pth = 'preprocessed\qst'
-ckpt_pth = 'late_fusion\ckpt'
-log_pth = 'late_fusion\log'
+img_pth = 'dataset/img'
+ann_pth = 'dataset/ann'
+qst_pth = 'dataset/qst'
+out_img_pth = 'preprocessed/img'
+out_data_pth = 'preprocessed/data'
+out_vocab_pth = 'preprocessed/vocab'
+out_ann_pth = 'preprocessed/ann'
+out_qst_pth = 'preprocessed/qst'
+ckpt_pth = 'late_fusion/ckpt'
 
-
-# Preprocess images
-
-# In[ ]:
-
-
-def resize_image(image, size):
-    """Resize an image to the given size."""
-    return image.resize(size, Image.Resampling.LANCZOS)
-
-def resize_images(input_dir, output_dir, size, split_ratio):
-    """Resize the images in 'input_dir' and save into 'output_dir'."""
-    for idir in os.scandir(input_dir):
-        if not idir.is_dir():
-            print('No valid directory')
-            continue
-        if not os.path.exists(output_dir+'\\'+idir.name):
-            os.makedirs(output_dir+'\\'+idir.name)
-        else:
-            for file in os.listdir(output_dir+'\\'+idir.name):
-                if os.path.isfile(os.path.join(output_dir+'\\'+idir.name, file)):
-                    os.remove(os.path.join(output_dir+'\\'+idir.name, file))
-                      
-        images = os.listdir(idir.path)
-        images, _ = train_test_split(images,
-                                     test_size=split_ratio,
-                                     shuffle=False)
-        n_images = len(images)
-        for id, image in enumerate(images):
-            try:
-                with open(os.path.join(idir.path, image), 'r+b') as f:
-                    with Image.open(f) as img:
-                        img = resize_image(img, size)
-                        img.save(os.path.join(output_dir+'\\'+idir.name, image), img.format)
-            except(IOError, SyntaxError) as e:
-                pass
-            if (id+1) % 500 == 0:
-                print("[{}/{}] resized images and saved into '{}'."
-                      .format(id+1, n_images, output_dir+'\\'+idir.name))
-                    
-def main():
-
-    input_dir = img_pth
-    output_dir = out_img_pth
-    image_size = [224, 224]
-    split_ratio = 0.95
-    resize_images(input_dir, output_dir, image_size, split_ratio)
-    
-main()
-
-
-# Removing questions and annotations that doesnt belong to the images
-
-# In[ ]:
-
-
-def removing(out_img_pth, ann_pth, qst_pth):
-
-    test_ids = []
-    train_ids = []
-    val_ids = []
-    for idir in os.scandir(out_img_pth):
-        for file in os.listdir(idir):
-            components = file.split('_')
-            image_id = components[-1].split('.')[0]
-            numeric_part = int(image_id)
-            if 'test' in idir.name:
-                test_ids.append(numeric_part)
-            elif 'train' in idir.name:
-                train_ids.append(numeric_part)
-            elif 'val' in idir.name:
-                val_ids.append(numeric_part)
-    
-    for idir in os.scandir(ann_pth):
-        if 'test' in idir.name:
-            ids = test_ids
-        elif 'train' in idir.name:
-            ids = train_ids
-        elif 'val' in idir.name:
-            ids = val_ids
-        
-        for file in os.listdir(idir):
-            path = os.path.join(idir, file)
-            with open(path, 'r') as f:
-                data = json.load(f)
-            annotations = data['annotations']
-            prelen = len(annotations)
-            labels = dict()
-            number = 0
-            for label in annotations:
-                    if int(label['image_id']) in ids:
-                        labels.update({number: label})   
-                        number += 1
-            print(f'{len(labels)} remaining annotations of {prelen} in {file}')
-            data['annotations'] = labels
-            with open(out_ann_pth + '\\' + file, 'w') as f:
-                json.dump(data, f)
-    
-    for idir in os.scandir(qst_pth):
-        if 'test' in idir.name:
-            ids = test_ids
-        elif 'train' in idir.name:
-            ids = train_ids
-        elif 'val' in idir.name:
-            ids = val_ids
-            
-        for file in os.listdir(idir):
-            path = os.path.join(idir, file)
-            with open(path, 'r') as f:
-                data = json.load(f)
-            questions = data['questions']
-            prelen = len(questions)
-            labels = dict()
-            number = 0
-            for label in questions:
-                    if int(label['image_id']) in ids:
-                        labels.update({number: label}) 
-                        number += 1
-            print(f'{len(labels)} remaining questions of {prelen} in {file}')
-            data['questions'] = labels
-            with open(out_qst_pth + '\\' + file, 'w') as f:
-                json.dump(data, f)
-
-def main():
-    img_pth = out_img_pth
-    annot_pth = ann_pth
-    quest_pth = qst_pth
-    removing(img_pth, annot_pth, quest_pth)
-    
-main()
-
-
-# Make vocab
-
-# In[ ]:
-
-
-def make_q_vocab(input_dir, output_dir):
-
-    for file in os.scandir(input_dir):
-        if "test" in file.name:
-            continue
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir) 
-            
-        regex = re.compile(r'(\W+)')
-        q_vocab = []
-        path = os.path.join(input_dir, file.name)
-        with open(path, 'r') as f:
-            q_data = json.load(f)
-        question = q_data['questions'].values()
-        for quest in question:
-            split = regex.split(quest['question'].lower())
-            tmp = [w.strip() for w in split if len(w.strip()) > 0]
-            q_vocab.extend(tmp)
-    
-        q_vocab = list(set(q_vocab))
-        q_vocab.sort()
-        q_vocab.insert(0, '<pad>')
-        q_vocab.insert(1, '<unk>')
-    
-        with open(output_dir + '\\' + file.name.split(".")[0] + '_vocabs.txt', 'w') as f:
-            f.writelines([v+'\n' for v in q_vocab])
-
-        print(f'The number of total words of questions in {file.name}: {len(q_vocab)}')
-
-def make_a_vocab(input_dir, output_dir):
-
-    for file in os.scandir(input_dir):
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir) 
-            
-        answers = defaultdict(lambda :0)
-        path = os.path.join(input_dir, file.name)
-        with open(path, 'r') as f:
-            data = json.load(f)
-        annotations = data['annotations'].values()
-        for label in annotations:
-            for ans in label['answers']:
-                vocab = ans['answer']
-                if re.search(r'[^\w\s]', vocab):
-                    continue
-                answers[vocab] += 1
-    
-        answers = sorted(answers, key=answers.get, reverse= True) 
-        with open(output_dir + '\\' + file.name.split(".")[0] + '_vocabs.txt', 'w') as f :
-            f.writelines([ans+'\n' for ans in answers])
-            
-        print(f'The number of total words of answers in {file.name}: {len(answers)}')
-
-def make_vocab(output_dir):
-    ann_vocab = set()
-    qst_vocab = set()
-    for file in os.scandir(output_dir):
-        if 'ann' in file.name:
-            with open(file.path, 'r') as f:
-                for line in f: 
-                    ann_vocab.add(line.split('\n')[0])
-        elif 'qst' in file.name:
-            with open(file.path, 'r') as f:
-                for line in f: 
-                    qst_vocab.add(line.split('\n')[0])
-
-    print(f'The number of total words of answers in ann_vocabs will be: {len(ann_vocab)}')
-    print(f'The number of total words of answers in qst_vocabs will be: {len(qst_vocab)}')
-    
-    with open(output_dir + '\\ann_vocabs.txt', 'w') as f:
-        f.writelines([ans+'\n' for ans in ann_vocab])
-    with open(output_dir + '\\qst_vocabs.txt', 'w') as f:
-        f.writelines([ans+'\n' for ans in qst_vocab])
-
-def main():
-    input_qst_dir = out_qst_pth
-    input_ann_dir = out_ann_pth
-    output_vocab_dir = out_vocab_pth
-    make_q_vocab(input_qst_dir, output_vocab_dir)
-    make_a_vocab(input_ann_dir, output_vocab_dir)
-    make_vocab(out_vocab_pth)
-main()
-    
-
-
-# Make Vocab
-
-# In[ ]:
 
 
 def preprocessing(image_dir, annotation_dir, question_dir, output_dir, vocab_dir):
@@ -358,11 +132,11 @@ def preprocessing(image_dir, annotation_dir, question_dir, output_dir, vocab_dir
         dataset.update({datatype: info})
         print(f'Total {match_top_ans.unk_ans} out of {len(questions)} answers are <unk>')
 
-    np.save(output_dir + '\\train.npy', np.array(dataset['train']))
-    np.save(output_dir + '\\val.npy', np.array(dataset['val']))
-    with open(output_dir + '\\train.json', 'w') as f:
+    np.save(output_dir + '/train.npy', np.array(dataset['train']))
+    np.save(output_dir + '/val.npy', np.array(dataset['val']))
+    with open(output_dir + '/train.json', 'w') as f:
         json.dump(dataset['train'], f)
-    with open(output_dir + '\\val.json', 'w') as f:
+    with open(output_dir + '/val.json', 'w') as f:
         json.dump(dataset['val'], f)
 
 def tokenizer(sentence):
