0cbbbf5af56a38021399e074a60a6770e133261b
DaniSode <danisode@student.chalmers.se>
2023-12-21 10:21:17 +0100

    aa

--- Code/models/training.py
+++ Code/models/training.py
@@ -269,9 +269,9 @@ def train():
             image = sample['image'].to(device=device)
             question = sample['question'].to(device=device)
             label = sample['answer'].to(device=device)
-            print(image)
-            print(question)
-            print(label)
+            print(tf.shape(image))
+            print(tf.shape(question))
+            print(tf.shape(label))
             # forward
             logits = model(image, question)
             print(logits)
