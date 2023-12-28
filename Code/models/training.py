2cfd663f3e63623f255822814fce538821557905
abhijeetdhillon41 <dhillonabhijeet41@gmail.com>
2023-12-28 18:01:53 +0100

    changed get_item

--- Code/models/training.py
+++ Code/models/training.py
@@ -86,7 +86,8 @@ class VQADataset(Dataset):
         sample = {'image': img, 'question': qu2idx, 'question_id': qu_id}
 
         ans2idx = [self.ans_vocab.word2idx(ans) for ans in self.input_data[idx]['valid_ans']]
-        ans2idx = np.random.choice(ans2idx, size=1, replace=False)
+        #ans2idx = np.random.choice(ans2idx, size=1, replace=False)
+        ans2idx = np.random.choice(ans2idx)
         sample['answer'] = ans2idx
 
         if self.transform:
