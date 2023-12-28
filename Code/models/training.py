53730610d8f2969c23cc2c3f0e6bb4c386cb8a97
abhijeetdhillon41 <dhillonabhijeet41@gmail.com>
2023-12-28 18:23:59 +0100

    change7

--- Code/models/training.py
+++ Code/models/training.py
@@ -91,8 +91,8 @@ class VQADataset(Dataset):
             ans2idx = [self.ans_vocab.word2idx(ans) for ans in self.input_data[idx]['valid_ans']]
             print(np.shape(ans2idx))
             #ans2idx = np.random.choice(ans2idx)
-            #ans2idx = np.random.choice(ans2idx, size=1, replace=False)[0]
-            #print(np.shape(ans2idx))
+            ans2idx = np.random.choice(ans2idx, size=1)
+            print(np.shape(ans2idx))
             sample['answer'] = ans2idx
         #old
         #ans2idx = [self.ans_vocab.word2idx(ans) for ans in self.input_data[idx]['valid_ans']]
