5d88a54152a8b5aaa9e9a4a94cfe67c867ddcf6e
abhijeetdhillon41 <dhillonabhijeet41@gmail.com>
2024-01-01 17:54:25 +0100

    Update training.py

--- Code/models/training.py
+++ Code/models/training.py
@@ -316,6 +316,7 @@ def train():
             image = sample['image'].to(device=device)
             question = sample['question'].to(device=device)
             label = sample['answer'].to(device=device)
+            print('label',label)
             
             
             #label = sample['answer'].squeeze().view(-1).to(device=device)
